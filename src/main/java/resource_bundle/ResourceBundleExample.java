94e05e7eb6e4abbad898fdb20e4fd440b26bedfb
isnagornov <isnagornov@yandex.ru>
2019-09-27 14:47:26 +0300

    resource bundle example

--- src/main/java/resource_bundle/ResourceBundleExample.java
+++ src/main/java/resource_bundle/ResourceBundleExample.java
@@ -8,30 +8,36 @@ public class ResourceBundleExample {
 
     public static void main(String[] args) {
 
+        Locale ruLocale = new Locale("ru", "RU");
+        Locale enLocale = new Locale("en", "EN");
+
         System.out.println("Current Locale - " + Locale.getDefault());
 
-        ResourceBundle defResourceBundle = ResourceBundle.getBundle("messages");
-        ResourceBundle ruResourceBundle = ResourceBundle.getBundle("messages_ru_RU");
-        ResourceBundle enResourceBundle = ResourceBundle.getBundle("messages_en_EN");
+        ResourceBundle propBundle = ResourceBundle.getBundle("messages");
 
         System.out.println("-----------------Getting values from properties bundle");
 
-        System.out.println("Message via default locale - " + defResourceBundle.getString("test-message"));
-        System.out.println("Message via ru locale - " + ruResourceBundle.getString("test-message"));
-        System.out.println("Message via ru locale - " + enResourceBundle.getString("test-message"));
+        System.out.println("Message via default locale - " + propBundle.getString("test-message"));
+
+        System.out.println("Message via ru locale - " + propBundle.getString("test-message"));
+
+        Locale.setDefault(enLocale);
+        propBundle = ResourceBundle.getBundle("messages");
+
+        System.out.println("Message via en locale - " + propBundle.getString("test-message"));
+
+        //get value that exist only in common messages.properties
+        System.out.println("Message from common messages - " + propBundle.getString("common-message"));
 
         System.out.println();
 
         System.out.println("-----------------Getting values from java bundle");
 
-        ResourceBundle listResourceBundle = ResourceBundle.getBundle("resource_bundle.CustomListResourceBundle");
         ResourceBundle listResourceBundleRU = ResourceBundle.getBundle("resource_bundle.CustomListResourceBundle_ru_RU");
         ResourceBundle listResourceBundleEN = ResourceBundle.getBundle("resource_bundle.CustomListResourceBundle_en_EN");
 
-        System.out.println("Message via default locale - " + listResourceBundle.getString("key1"));
         System.out.println("Message via ru locale - " + listResourceBundleRU.getString("key1"));
-        System.out.println("Message via ru locale - " + listResourceBundleEN.getString("key1"));
-
+        System.out.println("Message via en locale - " + listResourceBundleEN.getString("key1"));
 
     }
 }
